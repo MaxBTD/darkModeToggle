456cb7c0eefb924583fadd25dd1df15badff1a76
MaxBTD <97893081+MaxBTD@users.noreply.github.com>
2024-11-17 16:51:58 +0100

    darkMode Toggle ADDED

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  function darkModeSet(){
+    setDarkMode(!darkMode);
+    document.getElementsByTagName("body")[0].className = darkMode ? "darkMode" : "";
+  }
+
+  const DarkModeToggle = () => {
+    return(
+      <button darkMode={darkMode} onClick={() => {darkModeSet()}}>
+        Przełącz Tryb
+      </button>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget finibus nibh, id maximus dolor. 
+        Praesent nec lacus rhoncus, accumsan augue at, aliquam nibh. Maecenas rutrum metus scelerisque turpis facilisis efficitur. 
+        Aenean consectetur urna finibus, hendrerit neque et, pulvinar quam. Aenean rutrum massa neque, posuere aliquam mauris tincidunt sit amet. 
+        Vivamus nec libero odio. Ut in dictum felis. Donec rutrum interdum porttitor. Maecenas dignissim eros in justo porttitor aliquet. 
+        Duis blandit, odio eget vulputate convallis, nibh magna mattis tellus, id consectetur lectus urna sit amet nibh.</p>
+      <DarkModeToggle />
+      
     </div>
   );
 }
